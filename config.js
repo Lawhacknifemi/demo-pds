1eb6840dcc5d00b73436d9af1afe18e42cc25010
Sherif Lawal <lawalsheriffnifemi@gmail.com>
2025-05-09 12:55:12 +0100

    update did

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const BGS_SERVER = "bsky.network";
 // Instance-specific Configuration
 const HANDLE = "jpdshandle.memory-design.xyz";
 const PDS_SERVER = "jpd.memory-design.xyz";
-const DID_PLC = "did:plc:mbdzvqkfxdudqe2jhxo3mosj";
+const DID_PLC = "did:plc:hc4hffkuktpkcao6xh7jhizf";
 
 // Security Credentials
 const JWT_ACCESS_SECRET = "281e1a58a26cfbdbddcb183378174604b76bea9ffe6ff658c91cd3530613a829";
