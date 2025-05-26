2009c8705ac3ed8fe86327a5696e6e5775d9c079
Sherif Lawal <lawalsheriffnifemi@gmail.com>
2025-05-26 18:14:48 +0100

    fix websocket

---
+++ logger.js
@@ -0,0 +1,17 @@
+import winston from 'winston';
+
+// Initialize logging with Python-like format
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ level, message, timestamp }) => {
+            return `${timestamp} ${level.toUpperCase()}: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+export default logger; 
